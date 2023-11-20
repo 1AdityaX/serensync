a921fa161f0f688d081b6c85c02ba201810e84c0
1AdityaX <adityakb2005@gmail.com>
2023-11-20 22:12:40 +0530

    fixed lag issues while rendering

--- lib/apps.dart
+++ lib/apps.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:device_apps/device_apps.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:serensync/helpers/app_state.dart';
 
 class AppsPage extends StatefulWidget {
   const AppsPage({super.key});
@@ -10,48 +11,41 @@ class AppsPage extends StatefulWidget {
 }
 
 class _AppListScreenState extends State<AppsPage> {
-  Provider<List<Application>> _apps = Provider((ref) => []);
-
   @override
   void initState() {
     super.initState();
-    _loadApps();
-  }
-
-  Future<void> _loadApps() async {
-    List<Application> apps = await DeviceApps.getInstalledApplications(
-      onlyAppsWithLaunchIntent: true,
-      includeSystemApps: true,
-    );
-
-    apps.sort((a, b) => a.appName.compareTo(b.appName));
-    setState(() {
-      _apps = Provider((ref) => apps);
-    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Consumer(builder: (context, ref, child) {
-      final __apps = ref.watch(_apps);
+      final appsAsyncValue = ref.watch(appsProvider);
       return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      ),
-      body: ListView.builder(
-        itemCount: __apps.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(
-              __apps[index].appName,
-              style: const TextStyle(color: Colors.white, fontSize: 20),
-            ),
-            onTap: () => {DeviceApps.openApp(__apps[index].packageName)},
-          );
-        },
-        padding: const EdgeInsets.only(left: 20, top: 10, bottom: 30),
-      ),
-    );
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+          ),
+          body: appsAsyncValue.when(
+              loading: () {},
+              error: (error, stacktrace) {
+                return null;
+              },
+              data: (apps) {
+                return ListView.builder(
+                  itemCount: apps.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(
+                        apps[index].appName,
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                      onTap: () =>
+                          {DeviceApps.openApp(apps[index].packageName)},
+                    );
+                  },
+                  padding: const EdgeInsets.only(left: 20, top: 10, bottom: 30),
+                );
+              }));
     });
   }
 }
