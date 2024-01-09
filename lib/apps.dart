cd87a8a3248497db57cd24858810e0b9d87fc5d9
1AdityaX <adityakb2005@gmail.com>
2024-01-10 02:00:07 +0530

    refactory

--- lib/apps.dart
+++ lib/apps.dart
@@ -27,105 +27,112 @@ class AppListScreenState extends State<AppsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Container(
-            height: 45,
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 25, 25, 25),
-              borderRadius: BorderRadius.circular(23),
-            ),
-            child: Row(
-              children: [
-                ValueListenableBuilder(
-                    valueListenable: _isFocused,
-                    builder:
-                        (BuildContext context, bool isFocused, Widget? child) {
-                      return IconButton(
-                        onPressed: () {
-                          if (isFocused) {
-                            setState(() {
-                              _focusNode.unfocus();
-                              _controller.clear();
-                            });
-                          } else {
-                            _focusNode.requestFocus();
-                          }
-                        },
-                        icon: Icon(
-                          _focusNode.hasFocus ? Icons.arrow_back : Icons.search,
-                          color: Colors.white70,
-                        ),
-                      );
-                    }),
-                const SizedBox(width: 8),
-                Expanded(
-                  child: TextField(
-                    controller: _controller,
-                    focusNode: _focusNode,
-                    decoration: const InputDecoration(
-                      hintText: 'Search Apps',
-                      border: InputBorder.none,
-                      hintStyle: TextStyle(color: Colors.white70),
-                    ),
-                    onChanged: (query) {
+          title: _searchBar(),
+        ),
+        body: _appsList());
+  }
+
+  Widget _searchBar() {
+    return Container(
+      height: 45,
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 25, 25, 25),
+        borderRadius: BorderRadius.circular(23),
+      ),
+      child: Row(
+        children: [
+          ValueListenableBuilder(
+              valueListenable: _isFocused,
+              builder: (BuildContext context, bool isFocused, Widget? child) {
+                return IconButton(
+                  onPressed: () {
+                    if (isFocused) {
                       setState(() {
-                        _controller.text = query;
-                      });
-                    },
-                  ),
-                ),
-                IconButton(
-                    icon: _controller.text.isEmpty
-                        ? const Icon(Icons.settings)
-                        : const Icon(Icons.clear),
-                    onPressed: () {
-                      if (_controller.text.isNotEmpty) {
-                        setState(() {
-                          _controller.clear();
-                        });
-                      } else {
                         _focusNode.unfocus();
-                      }
-                    })
-              ],
+                        _controller.clear();
+                      });
+                    } else {
+                      _focusNode.requestFocus();
+                    }
+                  },
+                  icon: Icon(
+                    _focusNode.hasFocus ? Icons.arrow_back : Icons.search,
+                    color: Colors.white70,
+                  ),
+                );
+              }),
+          const SizedBox(width: 8),
+          Expanded(
+            child: TextField(
+              controller: _controller,
+              focusNode: _focusNode,
+              decoration: const InputDecoration(
+                hintText: 'Search Apps',
+                border: InputBorder.none,
+                hintStyle: TextStyle(color: Colors.white70),
+              ),
+              onChanged: (query) {
+                setState(() {
+                  _controller.text = query;
+                });
+              },
             ),
           ),
-        ),
-        body: Consumer(
-          builder: (context, ref, child) {
-            final appsAsyncValue = ref.watch(appsProvider);
-            return appsAsyncValue.when(loading: () {
-              return const Center(child: CircularProgressIndicator());
-            }, error: (error, stackTrace) {
-              return const Center(child: Text('Error'));
-            }, data: (apps) {
-              final filteredApps = apps
-                  .where((app) => app.appName
-                      .toLowerCase()
-                      .contains(_controller.text.toLowerCase()))
-                  .toList();
-              return ListView.builder(
-                itemCount: filteredApps.length,
-                itemBuilder: (buildContext, index) {
-                  return ListTile(
-                    title: Text(
-                      filteredApps[index].appName,
-                    ),
-                    onTap: () => {
-                      DeviceApps.openApp(filteredApps[index].packageName),
-                      setState(() {
-                        _controller.clear();
-                        _focusNode.unfocus();
-                      }),
-                    },
-                    onLongPress: () => showPopupDialog(filteredApps[index]),
-                  );
+          IconButton(
+              icon: _controller.text.isEmpty
+                  ? const Icon(Icons.settings)
+                  : const Icon(Icons.clear),
+              onPressed: () {
+                if (_controller.text.isNotEmpty) {
+                  setState(() {
+                    _controller.clear();
+                  });
+                } else {
+                  _focusNode.unfocus();
+                }
+              })
+        ],
+      ),
+    );
+  }
+
+  Widget _appsList() {
+    return Consumer(
+      builder: (context, ref, child) {
+        final appsAsyncValue = ref.watch(appsProvider);
+        return appsAsyncValue.when(loading: () {
+          return const Center(child: CircularProgressIndicator());
+        }, error: (error, stackTrace) {
+          return const Center(child: Text('Error'));
+        }, data: (apps) {
+          final filteredApps = apps
+              .where((app) => app.appName
+                  .toLowerCase()
+                  .contains(_controller.text.toLowerCase()))
+              .toList();
+          return ListView.builder(
+            itemCount: filteredApps.length,
+            itemBuilder: (buildContext, index) {
+              return ListTile(
+                title: Text(
+                  filteredApps[index].appName,
+                ),
+                onTap: () => {
+                  DeviceApps.openApp(filteredApps[index].packageName),
+                  setState(() {
+                    _controller.clear();
+                    _focusNode.unfocus();
+                  }),
                 },
-                padding: const EdgeInsets.only(left: 20),
+                onLongPress: () => showPopupDialog(filteredApps[index]),
               );
-            });
-          },
-        ));
+            },
+            padding: const EdgeInsets.only(left: 20),
+          );
+        });
+      },
+    );
   }
 
   Future<void> showPopupDialog(Application app) async => showDialog(
